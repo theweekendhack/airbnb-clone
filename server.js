131af7dd465c44129a32c6db04485209d9210313
kadeembestteaches <56241232+kadeembestteaches@users.noreply.github.com>
2020-07-24 22:45:29 -0400

    Added twilio sms and voice funcitonality

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 const bodyParser = require("body-parser");
+require('dotenv').config({path:"config/keys.env"})// defaults is to create a file called .env and place it on the root
 
 
 const app = express();
@@ -14,8 +15,12 @@ app.use(express.static("public")); //middleware
 app.use(bodyParser.urlencoded({ extended: false }))
 
 
+// Download the helper library from https://www.twilio.com/docs/node/install
+// Y
 // routes 
 app.get("/",(req,res)=>{
+
+    console.log(`${process.env.TWILIO_SID}`);
 
         console.log("THIS IS SERVER CODE!!!!!!");
     
@@ -46,19 +51,70 @@ app.get("/contact-us",(req,res)=>{
 
 })
 
+
+
 app.post("/contact-us",(req,res)=>{
 
 
-    console.log(req.body)
-    //console.log(`Name ${req.body.firstName}  ${req.body.lastName}`)
+    const accountSid = process.env.TWILIO_SID;
+    const authToken =  process.env.TWILIO_AUTH_TOKEN;
 
-});
+    const client = require('twilio')(accountSid, authToken);
+
+    client.messages
+        .create(
+        {
+                body: `Hi  ${req.body.firstName}  ${req.body.lastName}  . Thank you for registering with AirBNB. Someone from our company
+                will contact you shortly!!!! `,
+                from: '+14246250342', 
+                to: `${req.body.phone}`
+        })
+       
+        .then(message =>
+        {
+
+            //represents success!!!!! 
+            console.log(message.sid);
+
+            
+        //const accountSid = 'AC69322baa06308fb9b50071c5245626aa';
+        //const authToken = 'your_auth_token';
+       // const client = require('twilio')(accountSid, authToken);
+        
+        client.calls
+              .create({
+                 url: 'http://demo.twilio.com/docs/voice.xml',
+                 to: `${req.body.phone}`,
+                 from: '+14246250342'
+               })
+              .then(call => 
+                {
+                    console.log(call.sid);
+                    res.redirect("/welcome")
+
+                })
+              .catch(err=>console.log(`Error ${err}`));
+                
+    
+        })
+        .catch(err=>console.log(`Error ${err}`))
+        
+    });
+
+
+
+    app.get("/welcome",(req,res)=>{
+
+        res.render("dashboard"); //
+    })
 
 
 
 
-const PORT = 3000;
 
-app.listen(PORT,()=>{
-    console.log(`The Web Server is up and running`);
-});
+
+    const PORT = 3000;
+
+    app.listen(PORT,()=>{
+        console.log(`The Web Server is up and running`);
+    });
